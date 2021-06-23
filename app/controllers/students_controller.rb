f74ef66e88cf6620104c1c0f2f90cc3e3ec3deb9
Luciana Sugeta <lulusugeta@gmail.com>
2021-06-23 17:48:09 -0400

    commit tree 57865887125efa5f34d61a76756803cae378de2c

--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -1,11 +1,19 @@
+#Couldn't resolve the activate part by myself. Used the 'resolution' on repo to help me.
+
 class StudentsController < ApplicationController
-  before_action :set_student, only: :show
+  before_action :set_student, only: [:show, :activate]
   
   def index
     @students = Student.all
   end
 
   def show
+  end
+
+  def activate
+    @student.active = !@student.active
+    @student.save
+    redirect_to student_path(@student)
   end
 
   private
